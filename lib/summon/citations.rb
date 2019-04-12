3686cb67fdbc38b3009bef6a3501223c47cdd8f0
Josh Levinson <joshua.levinson@yale.edu>
2019-04-12 12:11:19 -0400

    Added specific functions for various types as well as their _text counterparts. Made most methods more generic.
    Probably should be refactored later but want to get something up and running asap.

--- lib/summon/citations.rb
+++ lib/summon/citations.rb
@@ -5,13 +5,37 @@ require "summon/citations/version"
 module Summon
   module Citations
     # also accepts 'refworks', 'endnote'
-    def to_citation(options = {}, format_type = 'ris')
+    def to_citation(format_Type = 'ris', options = {})
       Builder.build self, options, format_type
     end
 
-    def to_citations_text(options = {})
+    def to_refworks(options = {})
+      to_citation('refworks', options)
+    end
+
+    def to_refworks_text(options = {})
+      to_citation_text('refworks', options)
+    end
+
+    def to_endnote(options = {})
+      to_citation('endnote', options)
+    end
+
+    def to_endnote_text(options = {})
+      to_citation_text('endnote', options)
+    end
+
+    def to_ris(options = {})
+      to_citation(options)
+    end
+
+    def to_ris_text(options = {})
+      to_citation_text(options)
+    end
+
+    def to_citation_text(format_type = 'ris', options = {})
       buffer = StringIO.new
-      to_citation(options).each do |key, values|
+      to_citation(format_type, options).each do |key, values|
         values.each do |value|
           buffer.puts "#{key} #{value}"
         end
@@ -25,10 +49,12 @@ module Summon
       def self.build(document, options, format_type)
         document.extend self
         document.to_citation_tagged_format options
+        @options = options
+        @format_type = format_type
       end
 
-      def citations_tags(format_type)
-        filename = File.expand_path("../{#format_type}_mappings.rb", __FILE__)
+      def citations_tags
+        filename = File.expand_path("../#{@format_type}_mappings.rb", __FILE__)
         eval(File.read(filename), binding, filename, 1)
       end
 
@@ -40,14 +66,14 @@ module Summon
         end.compact
       end
 
-      def content_type_to_reference_type(format_type)
-        mapping = YAML.load(File.read File.expand_path("../{#format_type}.yaml", __FILE__))
+      def content_type_to_reference_type
+        mapping = YAML.load(File.read File.expand_path("../#{@format_type}.yaml", __FILE__))
         mapping[content_type] || 'Generic'
       end
 
-      def to_citation_tagged_format(options)
+      def to_citation_tagged_format
         {}.tap do |tags|
-          citations_tags.merge(options).each do |tag, mapping|
+          citations_tags.merge(@options).each do |tag, mapping|
             tags[tag] = citations_normal case mapping
             when Symbol then send(mapping)
             when Proc then mapping.call()
