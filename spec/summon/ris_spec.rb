0cf8dd30660206f06fd302a1b2ae57147c570b93
Lixia Zhao <LixiaZhao@sysops-MacBook-Pro.local>
2019-06-26 10:17:22 -0400

    add multiple value test

--- spec/summon/ris_spec.rb
+++ spec/summon/ris_spec.rb
@@ -16,4 +16,19 @@ describe Summon::Ris do
     expect(@doc.to_ris).to have_key(:'ER  -')
   end
 
+  describe "controlling how multiple values are concatenated" do
+    before do
+      @doc.stub(:multi_value) {[1,2,3]}
+    end
+
+    it "joins multiple values with '; ' by default" do
+      #@doc.to_ris(:multi_value => lambda {@doc.multi_value})[:multi_value].should eql ["1; 2; 3"]
+      expect(@doc.to_ris(:multi_value => lambda {@doc.multi_value})[:multi_value]).to eql (["1; 2; 3"])
+    end
+
+    it "accepts the option to preseve multiple values per field" do
+      #@doc.to_ris(:multi_value => lambda {@doc.multi_value.tag_per_value})[:multi_value].should eql [1,2,3]
+      expect(@doc.to_ris(:multi_value => lambda {@doc.multi_value.tag_per_value})[:multi_value]).to eql ([1, 2, 3])
+    end
+  end
 end
